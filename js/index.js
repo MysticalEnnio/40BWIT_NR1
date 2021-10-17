3552fe43fde06d6bc7e982d555656d3920e030ec
MysticalEnnio <mystical.ennio@gmail.com>
2021-10-17 18:01:27 +0200

    v0.6.0

--- js/index.js
+++ js/index.js
@@ -10,6 +10,33 @@
 //#region Main
 document.addEventListener("DOMContentLoaded", () => {
 
+  //#region darkmode
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme == 'dark') {
+        toggleSwitch.checked = true;
+    }
+  }
+
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+  }
+  
+  toggleSwitch.addEventListener('change', switchTheme, false);
+  //#endregion
+
   //#region vars
   var letterRegex = /[a-zA-Z]+/;
   var input;
